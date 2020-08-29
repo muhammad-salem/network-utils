feaf485a7b01d5cba056cd968998810409c60e2f
Mohamed Salem <salem.ebo@gmail.com>
2020-08-29 09:22:56 +0200

    add connectivity module

---
+++ connectivity/src/main/java/org/network/connectivity/ConnectivityCheck.java
@@ -0,0 +1,19 @@
+package org.network.connectivity;
+
+public interface ConnectivityCheck {
+
+    boolean isOnline();
+
+    /**
+     * Perform an HTTP GET request to check network connectivity
+     * if we got any response (code > 200)  --> connected <br>
+     * if ISP redirect the request			--> redirected <br>
+     * if got any network / IO exception 	--> disconnected <br>
+     *
+     * @return
+     */
+    NetworkStatus networkStatus();
+
+    NetworkReport networkReport();
+
+}
---
+++ connectivity/src/main/java/org/network/connectivity/NetworkStatus.java
@@ -0,0 +1,7 @@
+package org.network.connectivity;
+
+public enum NetworkStatus {
+    Connected,
+    Redirected,
+    Disconnected;
+}
